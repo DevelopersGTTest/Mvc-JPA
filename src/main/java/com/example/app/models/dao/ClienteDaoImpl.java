ebdb5327f064b1abf1f845cd982bea553a0f3425
unknown <pablojacobo1998@gmail.com>
2018-12-06 22:06:02 -0600

    Dao/Mapping Models

---
+++ src/main/java/com/example/app/models/dao/ClienteDaoImpl.java
@@ -0,0 +1,15 @@
+package com.example.app.models.dao;
+
+import java.util.List;
+
+import com.example.app.models.entity.Cliente;
+
+public class ClienteDaoImpl implements IClienteDao {
+
+	@Override
+	public List<Cliente> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/com/example/app/models/dao/IClienteDao.java
@@ -0,0 +1,5 @@
+package com.example.app.models.dao;
+
+public class IClienteDao {
+
+}
