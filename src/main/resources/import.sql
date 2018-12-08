d2c30d1945163a07ca29cfe52a96b0a31637a981
unknown <pablojacobo1998@gmail.com>
2018-12-07 21:30:32 -0600

    Adding Styles

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
 /* Populate tables */
-INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(1, 'Andre', 'Guzman', 'profesor@bolsadeideas.com', '2017-08-28');
-INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(2, 'John', 'Doe', 'john.doe@gmail.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(1, 'Pablo', 'Hackobo', 'hackobo@mail.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(2, 'Dvid', 'East', 'dveast@gmail.com', '2017-08-28');
